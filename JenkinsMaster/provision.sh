c0c2d531154043b5c8eb9af6708f8d780872117b
Mute <sashatsupanych@gmail.com>
2019-04-03 21:08:25 +0300

    added provision for jenkins

---
+++ JenkinsMaster/provision.sh
@@ -0,0 +1,14 @@
+#Installing Java and Jenkins 
+
+sudo apt update
+sudo apt install openjdk-8-jdk
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update
+sudo apt install jenkins
+
+#Configuring firewall
+
+sudo ufw allow 8080
